3861e1ca3ec4e980ff090877815360d9ec039445
Allan França <allanfd12@gmail.com>
2023-06-19 00:53:23 -0300

    add express

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
-const http = require('http');
-const port = 3000;
+import app from "./src/app.js";
+const port = process.env.PORT || 3000;
 
-
-const rotas = {
-    '/':'index',
-    '/contato':'contato',
-    '/sobre':'sobre'
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n'+ rotas[req.url]);
-
-});
-
-server.listen(port,() =>{
+app.listen(port,() =>{
     console.log("Server is running on port 3000")
 });
