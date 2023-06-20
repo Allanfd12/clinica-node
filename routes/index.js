0447af758a4e1f05ad4a5c9ea81b427b4f0d4a75
Gabriela Gomes <gamacarrerag@gmail.com>
2023-06-19 22:59:32 -0300

    Inclusão do modulo do mysql e criação de connexão com o banco.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var db = require('../database/connection'); // Conexão exposta
 var router = express.Router();
+
+console.log("Acessando!"); // mensagem para acesso ao banco 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
