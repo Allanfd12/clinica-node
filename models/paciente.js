596606f32e5fda2e15b0d6ca1de8ce84ecf8c48d
Gabriela Gomes <gamacarrerag@gmail.com>
2023-06-21 01:01:41 -0300

    Inclusão de select base pacientes na rota /pacientes

--- models/paciente.js
+++ models/paciente.js
@@ -1,7 +1,10 @@
+var db = require('../database/connection'); 
+
 class Paciente {
 
     static async find() {
-     
+        var pacientes = await global.connection.query('SELECT * FROM pacientes');
+        return pacientes[0];
     }
     
 }
