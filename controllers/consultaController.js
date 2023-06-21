a39b04778cec8da96ebc12516e31a157cb0da1b7
Sergio Mendes <srgmendesl@gmail.com>
2023-06-21 20:59:26 -0300

    retornando valores formatados em consulta

--- controllers/consultaController.js
+++ controllers/consultaController.js
@@ -1,10 +1,17 @@
 var Consulta = require('../models/consulta.js');
+const moment = require('moment');
 
 class ConsultaController {
 
     static async getAll(req, res) {
         try {
             let consultas = await Consulta.find();
+
+            consultas.forEach(consulta => {
+                consulta.data = moment(consulta.data).format('DD/MM/YYYY');
+                consulta.hora = moment(consulta.hora, 'HH:mm:ss').format('HH:mm');
+            });
+
             res.render('consulta', { consultas: consultas });
         } catch (error) {
             console.log(error);
